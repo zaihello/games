7db0c10b7c2eeed8d3ebc642167ee781ab944a0e
Zai <bluekine727@gmail.com>
2025-09-28 11:48:55 +0800

    first

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createRouter,createWebHashHistory } from 'vue-router'
+import './style.css'
+import App from './App.vue'
+import memoryMatchGame from './vueTemplate/memoryMatchGame.vue'
+
+
+const routes = [
+    {path:'/',component:memoryMatchGame}
+]
+
+const router = createRouter({
+    history:createWebHashHistory(),
+    routes,
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
